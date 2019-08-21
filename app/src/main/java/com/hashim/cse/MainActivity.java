581405dae2d5fc311a2941ec56ca92a2eae0005a
HashimJVZ <dailuotes@gmail.com>
2019-08-22 00:40:09 +0530

    Intialising

---
+++ app/src/main/java/com/hashim/cse/MainActivity.java
@@ -0,0 +1,22 @@
+package com.hashim.cse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.googlegroupfragment);
+
+        webView = findViewById(R.id.googlegroup);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://bit.ly/2Z1xAs6");
+    }
+}
